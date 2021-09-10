5e117dafe1e17db0172845133f25f4b8475de470
Usman Suleiman <usmansbk@gmail.com>
2021-09-10 22:19:45 +0100

    Style login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,17 @@
+import logo from "../../assets/logo-text.png";
+import classes from "./Login.module.css";
+
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <img src={logo} alt="" className={classes.logo} />
+        <div className={classes.body}>
+          <form className={classes.form}></form>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
