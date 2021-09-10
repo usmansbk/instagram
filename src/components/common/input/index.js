10237d96140d655556597867a8ed5a666f83fef3
Usman Suleiman <usmansbk@gmail.com>
2021-09-10 22:49:24 +0100

    Style input

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -3,7 +3,10 @@ import classes from "./index.module.css";
 const Input = ({ placeholder }) => {
   return (
     <div className={classes.container}>
-      <input className={classes.input} placeholder={placeholder} />
+      <label className={classes.label}>
+        <span className={classes.placeholder}>{placeholder}</span>
+        <input className={classes.input} placeholder={placeholder} />
+      </label>
     </div>
   );
 };
