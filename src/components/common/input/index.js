f3c966434f73c73646aa78d6c338982a0ebfd986
Usman Suleiman <usmansbk@gmail.com>
2021-09-10 23:45:54 +0100

    Set border

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -3,12 +3,16 @@ import clsx from "clsx";
 import classes from "./index.module.css";
 
 const Input = ({ placeholder }) => {
+  const [focused, setFocused] = useState(false);
   const [value, setValue] = useState("");
   const isFocused = !!value;
 
   return (
     <div
-      className={clsx(classes.container, isFocused && classes.containerFocused)}
+      className={clsx(
+        classes.container,
+        (focused || isFocused) && classes.containerFocused
+      )}
     >
       <label className={classes.label}>
         <span
@@ -22,6 +26,8 @@ const Input = ({ placeholder }) => {
         <input
           className={clsx(classes.input, isFocused && classes.inputFocus)}
           onChange={(e) => setValue(e.target.value)}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
           value={value}
         />
       </label>
