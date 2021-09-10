7d792e66a29093441a4c3c8db08e032848d7d8f2
Usman Suleiman <usmansbk@gmail.com>
2021-09-10 23:04:31 +0100

    Animate placeholder

--- src/components/common/input/index.js
+++ src/components/common/input/index.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
+import clsx from "clsx";
 import classes from "./index.module.css";
 
 const Input = ({ placeholder }) => {
+  const [focused, setFocused] = useState(false);
+  const onFocus = () => {
+    setFocused(true);
+  };
+
+  const onBlur = () => {
+    setFocused(false);
+  };
+
   return (
-    <div className={classes.container}>
+    <div
+      className={clsx(classes.container, focused && classes.containerFocused)}
+    >
       <label className={classes.label}>
-        <span className={classes.placeholder}>{placeholder}</span>
-        <input className={classes.input} />
+        <span
+          className={clsx(
+            classes.placeholder,
+            focused && classes.animatePlaceholder
+          )}
+        >
+          {placeholder}
+        </span>
+        <input className={classes.input} onFocus={onFocus} onBlur={onBlur} />
       </label>
     </div>
   );
