fb118c7e472122a7b76a9e403ecba64a54a4c89b
Usman Suleiman <usmansbk@gmail.com>
2021-09-11 19:33:51 +0100

    Use sprite

--- src/components/common/button/facebook/index.js
+++ src/components/common/button/facebook/index.js
@@ -1,10 +1,10 @@
+import Icon from "../../icon";
 import classes from "./index.module.css";
-import logo from "./facebook.svg";
 
 const FacebookTextButton = () => {
   return (
     <button className={classes.button}>
-      <img src={logo} alt="" className={classes.logo} />
+      <Icon name="facebookIcon" className={classes.logo} />
       <span className={classes.label}>Log in with Facebook</span>
     </button>
   );
