e203e1bd832e2d902e5fe3c1527c648bea7bd219
Usman Suleiman <usmansbk@gmail.com>
2021-09-11 22:14:06 +0100

    Style search box

--- src/components/home/header/index.js
+++ src/components/home/header/index.js
@@ -2,17 +2,30 @@ import { Link } from "react-router-dom";
 import classes from "./index.module.css";
 import logo from "./logo.png";
 
+const Logo = () => (
+  <div className={classes.left}>
+    <Link to="/" className={classes.link}>
+      <div className={classes.navBrand}>
+        <img src={logo} alt="" className={classes.logo} />
+      </div>
+    </Link>
+  </div>
+);
+
+const SearchInput = () => {
+  return (
+    <div className={classes.center}>
+      <input className={classes.searchInput} placeholder="Search" />
+    </div>
+  );
+};
+
 const Header = () => {
   return (
     <header className={classes.container}>
       <div className={classes.content}>
-        <div className={classes.left}>
-          <Link to="/" className={classes.link}>
-            <div className={classes.navBrand}>
-              <img src={logo} alt="" className={classes.logo} />
-            </div>
-          </Link>
-        </div>
+        <Logo />
+        <SearchInput />
       </div>
     </header>
   );
