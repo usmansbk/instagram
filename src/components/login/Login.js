a1d89a33b81e7946df12a31eb9751c0ad2e81b1a
Usman Suleiman <usmansbk@gmail.com>
2021-09-11 03:25:26 +0100

    Style signup link

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -8,30 +8,41 @@ import FacebookTextButton from "../common/button/facebook";
 const Login = () => {
   return (
     <div className={classes.container}>
-      <div className={classes.content}>
-        <TextLogo />
-        <div className={classes.body}>
-          <form className={classes.form}>
-            <div className={classes.formContent}>
-              <div className={classes.inputs}>
-                <Input placeholder="Phone number, username or email address" />
-                <Input placeholder="Password" />
+      <div>
+        <div className={classes.content}>
+          <TextLogo />
+          <div className={classes.body}>
+            <form className={classes.form}>
+              <div className={classes.formContent}>
+                <div className={classes.inputs}>
+                  <Input placeholder="Phone number, username or email address" />
+                  <Input placeholder="Password" />
+                </div>
+                <div className={classes.button}>
+                  <Button value="Log In" disabled />
+                </div>
+                <Or />
+                <div className={classes.oauth}>
+                  <FacebookTextButton />
+                </div>
               </div>
-              <div className={classes.button}>
-                <Button value="Log In" disabled />
-              </div>
-              <Or />
-              <div className={classes.oauth}>
-                <FacebookTextButton />
-              </div>
-            </div>
-          </form>
-          <a href="/forgot" className={classes.link}>
-            Forgotten your password?
-          </a>
+            </form>
+            <a href="/forgot" className={classes.link}>
+              Forgotten your password?
+            </a>
+          </div>
+        </div>
+        <div className={classes.signup}>
+          <div className={classes.signupContent}>
+            <p className={classes.signupText}>
+              Don't have an account?{" "}
+              <a className={classes.signupLink} href="/signup">
+                Sign up
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-      <div className={classes.signup}></div>
     </div>
   );
 };
