fb0d3008500967f8a503cb759654f915a9da9465
Usman Suleiman <usmansbk@gmail.com>
2021-09-11 04:26:16 +0100

    Setup router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/login";
+import Signup from "./components/signup";
 
 function App() {
   return (
-    <Login />
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
