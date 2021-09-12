e8d756a1ac92ce47cb4b6f859b8d2bc27c36b222
Usman Suleiman <usmansbk@gmail.com>
2021-09-12 11:38:40 +0100

    Fix icon

--- src/components/common/icon/index.js
+++ src/components/common/icon/index.js
@@ -3,7 +3,7 @@ import classes from "./index.module.css";
 import "./icons.css";
 
 const Icon = ({ name, className }) => (
-  <span className={clsx(classes.asset, name, className)} />
+  <div className={clsx(classes.asset, name, className)} />
 );
 
 export default Icon;
