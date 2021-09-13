279c3223b853de43b4bbb0ed4edd5c4af3323ba6
Usman Suleiman <usmansbk@gmail.com>
2021-09-13 13:10:04 +0100

    Add overlay

--- src/components/home/header/menu/index.js
+++ src/components/home/header/menu/index.js
@@ -1,7 +1,11 @@
 import classes from "./index.module.css";
 
 const Menu = () => {
-  return <div className={classes.container}></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.overlay} />
+    </div>
+  );
 };
 
 export default Menu;
